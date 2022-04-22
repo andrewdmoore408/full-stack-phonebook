57f59368fa04646fb1034bbb594eab6c228e4119
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-21 23:17:40 -0400

    Add route to retrieve one contact
    GET /api/persons/:id now returns the contact if found or 404 if not found

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const requestedContact = persons.find(person => person.id === id);
+
+  if (requestedContact) {
+    response.json(requestedContact);
+  } else {
+    response.statusMessage = `No contact has id ${id}`;
+    response.status(404).end();
+  }
+});
+
 app.get('/info', (request, response) => {
   const timestamp = new Date();
 
