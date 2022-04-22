4b03ae6f75ddf410759c1b583c9ca0f1b160e349
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-22 00:17:12 -0400

    Update morgan logging to include request body

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 app.use(express.json());
 
 const morgan = require('morgan');
-app.use(morgan('tiny'));
+
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 const nextId = () => {
   const currentIds = persons.map(person => person.id);
